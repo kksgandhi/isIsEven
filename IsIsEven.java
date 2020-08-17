aba9d9d9b188f35363b7f5ae42c370d7f3bec775
kutub gandhi <kksgandhi@gmail.com>
2020-08-16 23:43:35 -0500

    add test for Integer.MAX_VALUE and add comments

--- IsIsEven.java
+++ IsIsEven.java
@@ -7,7 +7,11 @@ public class IsIsEven {
         }
 
         public static boolean isIsEven(Predicate<Integer> maybeIsEven) {
+                // Create a known working isEven function to test against
                 Predicate<Integer> workingIsEven = (i) -> (i % 2 == 0);
+                // Test Integer.MAX_VALUE specifically, it'll be excluded in the stream
+                if (maybeIsEven.test(Integer.MAX_VALUE)) return false;
+                // test every other number, ensure they all match
                 return IntStream
                         .range(Integer.MIN_VALUE, Integer.MAX_VALUE)
                         .allMatch((i) -> maybeIsEven.test(i) == workingIsEven.test(i));
